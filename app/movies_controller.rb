14ac507ccdff2eb7785230543b18c9731d10864a
Kachel Wittig <kjwittig@gmail.com>
2018-06-27 00:16:01 +0000

    Done.

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -63,9 +63,9 @@ end
 
 def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick"
-  movie = Movie.create(title: "Awesome Flick")
-  Movie.find_by(title: "Awesome Flick")
-  movie.title = "Even Awesomer Flick"
+  Movie.create(title: "Awesome Flick")
+  movie = Movie.find_by(title: "Awesome Flick")
+  movie.update(title: "Even Awesomer Flick")
   movie.save
 end
 
